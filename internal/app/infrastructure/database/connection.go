ed147022bdd5f53745f106b7fd4c432eeb8f9b25
ignacior <ignacioruedaboada@hotmail.com>
2024-01-11 16:49:35 -0500

    feat: add db connection

---
+++ internal/app/infrastructure/database/connection.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type DBConfig struct {
+	Name     string
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
+func (c DBConfig) DSN() string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s",
+		c.Username,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.Name,
+	)
+}
+
+func CreateConnection(
+	config DBConfig,
+) (*gorm.DB, error) {
+	db, err := gorm.Open(mysql.Open(config.DSN()), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
