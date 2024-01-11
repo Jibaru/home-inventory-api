0641800271e31da707957d3de5116f47768d7d4b
ignacior <ignacioruedaboada@hotmail.com>
2024-01-11 16:49:52 -0500

    feat: update health controller

--- internal/app/infrastructure/controllers/health.go
+++ internal/app/infrastructure/controllers/health.go
@@ -1,27 +1,35 @@
 package controllers
 
 import (
+	"github.com/jibaru/home-inventory-api/m/internal/app/application/services"
+	"github.com/jibaru/home-inventory-api/m/internal/app/infrastructure/responses"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 type HealthController struct {
-	Version string
+	versionService *services.VersionService
 }
 
 type HealthControllerResponse struct {
 	Version string `json:"version"`
 }
 
-func NewHealthController(version string) *HealthController {
-	return &HealthController{
-		version,
-	}
+func NewHealthController(versionService *services.VersionService) *HealthController {
+	return &HealthController{versionService}
 }
 
 func (c *HealthController) Handle(ctx echo.Context) error {
+	version, err := c.versionService.GetLatestVersion()
+	if err != nil {
+		return ctx.JSON(
+			http.StatusBadRequest,
+			responses.MessageResponse{Message: err.Error()},
+		)
+	}
+
 	return ctx.JSON(
 		http.StatusOK,
-		HealthControllerResponse{c.Version},
+		HealthControllerResponse{version.Value},
 	)
 }
