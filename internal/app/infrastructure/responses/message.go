1278a584b0f11f010e0e655ba8b5bc3a1f022499
jibaru <ignacioruedaboada@hotmail.com>
2024-01-11 22:53:29 -0500

    feat: add message response constructor

--- internal/app/infrastructure/responses/message.go
+++ internal/app/infrastructure/responses/message.go
@@ -3,3 +3,9 @@ package responses
 type MessageResponse struct {
 	Message string `json:"message"`
 }
+
+func NewMessageResponse(message string) MessageResponse {
+	return MessageResponse{
+		Message: message,
+	}
+}
