a008af6b050dbc8e420735075b68f842673d6f2f
jibaru <ignacioruedaboada@hotmail.com>
2024-01-12 22:06:33 -0500

    feat: update user service

--- internal/app/application/services/user.go
+++ internal/app/application/services/user.go
@@ -2,16 +2,16 @@ package services
 
 import (
 	"errors"
-	"github.com/jibaru/home-inventory-api/m/internal/app/domain/dao"
 	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/repositories"
 )
 
 type UserService struct {
-	userDAO dao.UserDAO
+	userRepository repositories.UserRepository
 }
 
-func NewUserService(userDAO dao.UserDAO) *UserService {
-	return &UserService{userDAO}
+func NewUserService(userRepository repositories.UserRepository) *UserService {
+	return &UserService{userRepository}
 }
 
 func (s *UserService) CreateUser(email, password string) (*entities.User, error) {
@@ -20,7 +20,7 @@ func (s *UserService) CreateUser(email, password string) (*entities.User, error)
 		return nil, err
 	}
 
-	err = s.userDAO.Create(user)
+	err = s.userRepository.Create(user)
 	if err != nil {
 		return nil, errors.New("cannot create user")
 	}
