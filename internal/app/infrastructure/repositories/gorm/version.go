c7885e63c7b571588544991b36c1b6f56580d554
jibaru <ignacioruedaboada@hotmail.com>
2024-01-12 22:06:03 -0500

    feat: add gorm version repository

---
+++ internal/app/infrastructure/repositories/gorm/version.go
@@ -0,0 +1,33 @@
+package gorm
+
+import (
+	"errors"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/repositories"
+	"gorm.io/gorm"
+)
+
+type VersionRepository struct {
+	db *gorm.DB
+}
+
+func NewVersionRepository(db *gorm.DB) *VersionRepository {
+	return &VersionRepository{
+		db,
+	}
+}
+
+func (r *VersionRepository) Create(version entities.Version) {
+	r.db.Create(&version)
+}
+
+func (r *VersionRepository) GetLatest() (*entities.Version, error) {
+	version := &entities.Version{}
+	err := r.db.First(version).Error
+
+	if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, repositories.ErrVersionNotFound
+	}
+
+	return version, nil
+}
