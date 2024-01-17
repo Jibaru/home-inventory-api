3a312ebb1ba211bef65f2006d73f44d7544f28a7
jibaru <ignacioruedaboada@hotmail.com>
2024-01-16 22:35:39 -0500

    feat: update user repository interface

--- internal/app/domain/repositories/user.go
+++ internal/app/domain/repositories/user.go
@@ -1,7 +1,16 @@
 package repositories
 
-import "github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+import (
+	"errors"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+)
+
+var (
+	ErrUserNotFound  = errors.New("user not found")
+	ErrCanNotGetUser = errors.New("can not get user")
+)
 
 type UserRepository interface {
 	Create(user *entities.User) error
+	FindByEmail(email string) (*entities.User, error)
 }
