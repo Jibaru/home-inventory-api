c8600312e565b37cd7a167b9bd2c155713abac0c
ignacior <ignacioruedaboada@hotmail.com>
2024-01-17 12:20:03 -0500

    feat: update jwt token generator test

--- internal/app/infrastructure/auth/jwt/jwt_generator_test.go
+++ internal/app/infrastructure/auth/jwt/jwt_generator_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestGenerateToken(t *testing.T) {
+func TestGenerateAndDecodeToken(t *testing.T) {
 	secret := "secret-key"
 	expirationTime := time.Hour
 	jwtGenerator := NewJwtGenerator(secret, expirationTime)
@@ -26,4 +26,25 @@ func TestGenerateToken(t *testing.T) {
 	assert.Equal(t, id, decodedClaims.ID)
 	assert.Equal(t, email, decodedClaims.Email)
 	assert.WithinDuration(t, time.Now().Add(expirationTime), time.Unix(decodedClaims.ExpiresAt.Unix(), 0), 5*time.Second)
+}
+
+func TestParseToken(t *testing.T) {
+	secret := "secret-key"
+	expirationTime := time.Hour
+	jwtGenerator := NewJwtGenerator(secret, expirationTime)
+
+	id := uuid.NewString()
+	email := "test@example.com"
+
+	token, err := jwtGenerator.GenerateToken(id, email)
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, token)
+
+	data, err := jwtGenerator.ParseToken(token)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, data)
+	assert.Equal(t, id, data.ID)
+	assert.Equal(t, email, data.Email)
 }
