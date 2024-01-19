d1f22316f85208a5c977646332289d3040b0ab30
ignacior <ignacioruedaboada@hotmail.com>
2024-01-19 12:03:27 -0500

    feat: add create rooms table migration

---
+++ migrations/20240117211440_create_rooms_table.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS rooms (
+    id CHAR(36) NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(255) NULL,
+    user_id CHAR(36) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT rooms_user_id_fk FOREIGN KEY (user_id) REFERENCES users(id)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE rooms;
+-- +goose StatementEnd
