947d4c96bbc1eb12e414f054cad99dc1ef61eaf1
jibaru <ignacioruedaboada@hotmail.com>
2024-01-21 15:32:34 -0500

    feat: add stub asset repository

---
+++ internal/app/infrastructure/repositories/stub/asset.go
@@ -0,0 +1,29 @@
+package stub
+
+import (
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/repositories"
+	"github.com/stretchr/testify/mock"
+)
+
+type AssetRepositoryMock struct {
+	mock.Mock
+}
+
+func (r *AssetRepositoryMock) Create(asset *entities.Asset) error {
+	args := r.Called(asset)
+	return args.Error(0)
+}
+
+func (r *AssetRepositoryMock) FindByEntity(
+	entity entities.Entity,
+	page *repositories.PageFilter,
+) ([]*entities.Asset, error) {
+	args := r.Called(entity, page)
+
+	if data := args.Get(0); data != nil {
+		return data.([]*entities.Asset), args.Error(1)
+	}
+
+	return nil, args.Error(1)
+}
