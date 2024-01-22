d4df4be546ee64c29906e3ded1094c829240f527
ignacior <ignacioruedaboada@hotmail.com>
2024-01-22 10:10:10 -0500

    feat: add identifiable entity

--- internal/app/domain/entities/entity.go
+++ internal/app/domain/entities/entity.go
@@ -4,3 +4,19 @@ type Entity interface {
 	EntityID() string
 	EntityName() string
 }
+
+type IdentifiableEntity struct {
+	ID string
+}
+
+func NewIdentifiableEntity(id string) *IdentifiableEntity {
+	return &IdentifiableEntity{id}
+}
+
+func (e *IdentifiableEntity) EntityID() string {
+	return e.ID
+}
+
+func (e *IdentifiableEntity) EntityName() string {
+	return "mock_entity"
+}
