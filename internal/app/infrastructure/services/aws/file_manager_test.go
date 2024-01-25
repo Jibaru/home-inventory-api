49898a77a768f1b94eed6816505433d859e0a833
ignacior <ignacioruedaboada@hotmail.com>
2024-01-25 16:34:04 -0500

    feat: update aws file manager test

--- internal/app/infrastructure/services/aws/file_manager_test.go
+++ internal/app/infrastructure/services/aws/file_manager_test.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/services"
 	"github.com/labstack/gommon/random"
 	"github.com/stretchr/testify/assert"
 	"log"
@@ -53,4 +54,55 @@ func TestFileManagerGenerateUrl(t *testing.T) {
 	url := manager.GenerateUrl(id, extension)
 
 	assert.Equal(t, expectedUrl, url)
+}
+
+func TestFileManagerDelete(t *testing.T) {
+	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	region := os.Getenv("AWS_REGION")
+	bucketName := os.Getenv("S3_BUCKET_NAME")
+	filePath := os.Getenv("FILE_MANAGER_TEST_FILE_PATH")
+	validObjectID := os.Getenv("FILE_MANAGER_TEST_VALID_OBJECT_ID")
+	validExtension := os.Getenv("FILE_MANAGER_TEST_VALID_EXTENSION")
+
+	if accessKey == "" ||
+		secretKey == "" ||
+		region == "" ||
+		bucketName == "" ||
+		filePath == "" {
+		log.Println("all required environment variables are not set")
+		return
+	}
+
+	manager := NewFileManager(accessKey, secretKey, region, bucketName)
+
+	err := manager.Delete(validObjectID, validExtension)
+
+	assert.NoError(t, err)
+}
+
+func TestFileManagerDeleteErrorCanNotDeleteFile(t *testing.T) {
+	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+	region := os.Getenv("AWS_REGION")
+	bucketName := os.Getenv("S3_BUCKET_NAME")
+	filePath := os.Getenv("FILE_MANAGER_TEST_FILE_PATH")
+	invalidObjectID := os.Getenv("FILE_MANAGER_TEST_INVALID_OBJECT_ID")
+	invalidExtension := os.Getenv("FILE_MANAGER_TEST_INVALID_EXTENSION")
+
+	if accessKey == "" ||
+		secretKey == "" ||
+		region == "" ||
+		bucketName == "" ||
+		filePath == "" {
+		log.Println("all required environment variables are not set")
+		return
+	}
+
+	manager := NewFileManager(accessKey, secretKey, region, bucketName)
+
+	err := manager.Delete(invalidObjectID, invalidExtension)
+
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, services.ErrCanNotDeleteFile)
 }
