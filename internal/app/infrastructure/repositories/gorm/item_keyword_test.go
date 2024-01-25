ddcd8a1f0f4914ef993bd278156031aaf95957e3
ignacior <ignacioruedaboada@hotmail.com>
2024-01-25 16:31:22 -0500

    feat: add gorm item keyword repository test

---
+++ internal/app/infrastructure/repositories/gorm/item_keyword_test.go
@@ -0,0 +1,105 @@
+package gorm
+
+import (
+	"errors"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/repositories"
+	"github.com/labstack/gommon/random"
+	"github.com/stretchr/testify/assert"
+	"regexp"
+	"testing"
+	"time"
+)
+
+func TestItemKeywordRepositoryCreateMany(t *testing.T) {
+	db, dbMock := makeDBMock()
+	itemKeywordRepository := NewItemKeywordRepository(db)
+
+	itemKeywords := []*entities.ItemKeyword{
+		{
+			ID:        uuid.NewString(),
+			ItemID:    uuid.NewString(),
+			Value:     random.String(10, random.Alphanumeric),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		{
+			ID:        uuid.NewString(),
+			ItemID:    uuid.NewString(),
+			Value:     random.String(10, random.Alphanumeric),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	}
+
+	dbMock.ExpectBegin()
+	dbMock.ExpectExec(regexp.QuoteMeta("INSERT INTO `item_keywords` (`id`,`value`,`item_id`,`created_at`,`updated_at`) VALUES (?,?,?,?,?),(?,?,?,?,?)")).
+		WithArgs(
+			itemKeywords[0].ID,
+			itemKeywords[0].Value,
+			itemKeywords[0].ItemID,
+			itemKeywords[0].CreatedAt,
+			itemKeywords[0].UpdatedAt,
+			itemKeywords[1].ID,
+			itemKeywords[1].Value,
+			itemKeywords[1].ItemID,
+			itemKeywords[1].CreatedAt,
+			itemKeywords[1].UpdatedAt,
+		).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	dbMock.ExpectCommit()
+
+	err := itemKeywordRepository.CreateMany(itemKeywords)
+
+	assert.NoError(t, err)
+	err = dbMock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
+
+func TestItemKeywordRepositoryCreateManyErrorCanNotCreateItemKeywords(t *testing.T) {
+	db, dbMock := makeDBMock()
+	itemKeywordRepository := NewItemKeywordRepository(db)
+
+	itemKeywords := []*entities.ItemKeyword{
+		{
+			ID:        uuid.NewString(),
+			ItemID:    uuid.NewString(),
+			Value:     random.String(10, random.Alphanumeric),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+		{
+			ID:        uuid.NewString(),
+			ItemID:    uuid.NewString(),
+			Value:     random.String(10, random.Alphanumeric),
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	}
+
+	dbMock.ExpectBegin()
+	dbMock.ExpectExec(regexp.QuoteMeta("INSERT INTO `item_keywords` (`id`,`value`,`item_id`,`created_at`,`updated_at`) VALUES (?,?,?,?,?),(?,?,?,?,?)")).
+		WithArgs(
+			itemKeywords[0].ID,
+			itemKeywords[0].Value,
+			itemKeywords[0].ItemID,
+			itemKeywords[0].CreatedAt,
+			itemKeywords[0].UpdatedAt,
+			itemKeywords[1].ID,
+			itemKeywords[1].Value,
+			itemKeywords[1].ItemID,
+			itemKeywords[1].CreatedAt,
+			itemKeywords[1].UpdatedAt,
+		).
+		WillReturnError(errors.New("database error"))
+	dbMock.ExpectRollback()
+
+	err := itemKeywordRepository.CreateMany(itemKeywords)
+
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, repositories.ErrCanNotCreateItemKeywords)
+	err = dbMock.ExpectationsWereMet()
+	assert.NoError(t, err)
+}
