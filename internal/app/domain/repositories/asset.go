804e29b5b1ae4ff8b69d58fc7d01e355b37525e3
ignacior <ignacioruedaboada@hotmail.com>
2024-01-25 16:32:11 -0500

    feat: update asset repository interface

--- internal/app/domain/repositories/asset.go
+++ internal/app/domain/repositories/asset.go
@@ -8,9 +8,11 @@ import (
 var (
 	ErrCanNotCreateAsset = errors.New("can not create asset")
 	ErrCanNotGetAssets   = errors.New("can not get assets")
+	ErrCanNotDeleteAsset = errors.New("can not delete asset")
 )
 
 type AssetRepository interface {
 	Create(asset *entities.Asset) error
 	FindByEntity(entity entities.Entity, page *PageFilter) ([]*entities.Asset, error)
+	Delete(id string) error
 }
