dc1660ee3507c4842eada8285e392b396fa7dac7
ignacior <ignacioruedaboada@hotmail.com>
2024-01-25 16:30:19 -0500

    feat: add gorm item keyword repository

---
+++ internal/app/infrastructure/repositories/gorm/item_keyword.go
@@ -0,0 +1,25 @@
+package gorm
+
+import (
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/repositories"
+	"gorm.io/gorm"
+)
+
+type ItemKeywordRepository struct {
+	db *gorm.DB
+}
+
+func NewItemKeywordRepository(db *gorm.DB) *ItemKeywordRepository {
+	return &ItemKeywordRepository{
+		db,
+	}
+}
+
+func (r *ItemKeywordRepository) CreateMany(itemKeywords []*entities.ItemKeyword) error {
+	if err := r.db.Create(&itemKeywords).Error; err != nil {
+		return repositories.ErrCanNotCreateItemKeywords
+	}
+
+	return nil
+}
