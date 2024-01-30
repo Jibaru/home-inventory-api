12af5165f93264ca2ce9147307d12e98c2438d51
ignacior <ignacioruedaboada@hotmail.com>
2024-01-30 12:36:13 -0500

    feat: update stub item repository

--- internal/app/infrastructure/repositories/stub/item.go
+++ internal/app/infrastructure/repositories/stub/item.go
@@ -13,3 +13,13 @@ func (r *ItemRepositoryMock) Create(item *entities.Item) error {
 	args := r.Called(item)
 	return args.Error(0)
 }
+
+func (r *ItemRepositoryMock) GetByID(id string) (*entities.Item, error) {
+	args := r.Called(id)
+
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+
+	return args.Get(0).(*entities.Item), args.Error(1)
+}
