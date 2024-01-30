a58189c5402ac233c4a5b378397202394877adf3
ignacior <ignacioruedaboada@hotmail.com>
2024-01-30 12:36:30 -0500

    feat: update box repository interface

--- internal/app/domain/repositories/box.go
+++ internal/app/domain/repositories/box.go
@@ -6,9 +6,17 @@ import (
 )
 
 var (
-	ErrCanNotCreateBox = errors.New("can not create box")
+	ErrCanNotCreateBox                         = errors.New("can not create box")
+	ErrBoxRepositoryBoxItemNotFound            = errors.New("box item not found")
+	ErrBoxRepositoryCanBotCreateBoxItem        = errors.New("can not create box item")
+	ErrBoxRepositoryCanNotUpdateBoxItem        = errors.New("can not update box item")
+	ErrBoxRepositoryCanNotCreateBoxTransaction = errors.New("can not create box transaction")
 )
 
 type BoxRepository interface {
 	Create(box *entities.Box) error
+	GetBoxItem(boxID string, itemID string) (*entities.BoxItem, error)
+	CreateBoxItem(boxItem *entities.BoxItem) error
+	UpdateBoxItem(boxItem *entities.BoxItem) error
+	CreateBoxTransaction(boxTransaction *entities.BoxTransaction) error
 }
