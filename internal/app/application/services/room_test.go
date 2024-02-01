5fed00a40e8ad7dd20eec20f864ab4c6191c18fa
jibaru <ignacioruedaboada@hotmail.com>
2024-01-31 20:45:31 -0500

    feat: update room service test

--- internal/app/application/services/room_test.go
+++ internal/app/application/services/room_test.go
@@ -3,6 +3,7 @@ package services
 import (
 	"errors"
 	"github.com/google/uuid"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
 	"github.com/jibaru/home-inventory-api/m/internal/app/infrastructure/repositories/stub"
 	"github.com/labstack/gommon/random"
 	"github.com/stretchr/testify/assert"
@@ -45,6 +46,97 @@ func TestRoomServiceCreateRoomErrorInRepository(t *testing.T) {
 
 	assert.Error(t, err)
 	assert.Nil(t, room)
+	assert.EqualError(t, err, mockError.Error())
+	roomRepository.AssertExpectations(t)
+}
+
+func TestRoomServiceGetAll(t *testing.T) {
+	roomRepository := new(stub.RoomRepositoryMock)
+	roomService := NewRoomService(roomRepository)
+
+	search := random.String(100, random.Alphanumeric)
+	userID := uuid.NewString()
+	pageFilter := PageFilter{
+		Page: 1,
+		Size: 10,
+	}
+
+	rooms := []*entities.Room{
+		{
+			ID:          uuid.NewString(),
+			Name:        random.String(100, random.Alphanumeric),
+			Description: nil,
+			UserID:      userID,
+		},
+	}
+	roomRepository.On("GetByQueryFilters", mock.AnythingOfType("repositories.QueryFilter"), mock.AnythingOfType("*repositories.PageFilter")).
+		Return(rooms, nil)
+
+	result, err := roomService.GetAll(search, userID, pageFilter)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, rooms, result)
+	roomRepository.AssertExpectations(t)
+}
+
+func TestRoomServiceGetAllErrorInRepository(t *testing.T) {
+	roomRepository := new(stub.RoomRepositoryMock)
+	roomService := NewRoomService(roomRepository)
+
+	search := random.String(100, random.Alphanumeric)
+	userID := uuid.NewString()
+	pageFilter := PageFilter{
+		Page: 1,
+		Size: 10,
+	}
+
+	mockError := errors.New("repository error")
+	roomRepository.On(
+		"GetByQueryFilters",
+		mock.AnythingOfType("repositories.QueryFilter"),
+		mock.AnythingOfType("*repositories.PageFilter"),
+	).Return(nil, mockError)
+
+	result, err := roomService.GetAll(search, userID, pageFilter)
+
+	assert.Error(t, err)
+	assert.Nil(t, result)
+	assert.EqualError(t, err, mockError.Error())
+	roomRepository.AssertExpectations(t)
+}
+
+func TestRoomServiceCountAll(t *testing.T) {
+	roomRepository := new(stub.RoomRepositoryMock)
+	roomService := NewRoomService(roomRepository)
+
+	search := random.String(100, random.Alphanumeric)
+	userID := uuid.NewString()
+
+	count := int64(10)
+	roomRepository.On("CountByQueryFilters", mock.AnythingOfType("repositories.QueryFilter")).Return(count, nil)
+
+	result, err := roomService.CountAll(search, userID)
+
+	assert.NoError(t, err)
+	assert.Equal(t, count, result)
+	roomRepository.AssertExpectations(t)
+}
+
+func TestRoomServiceCountAllErrorInRepository(t *testing.T) {
+	roomRepository := new(stub.RoomRepositoryMock)
+	roomService := NewRoomService(roomRepository)
+
+	search := random.String(100, random.Alphanumeric)
+	userID := uuid.NewString()
+
+	mockError := errors.New("repository error")
+	roomRepository.On("CountByQueryFilters", mock.AnythingOfType("repositories.QueryFilter")).Return(int64(0), mockError)
+
+	result, err := roomService.CountAll(search, userID)
+
+	assert.Error(t, err)
+	assert.Equal(t, int64(0), result)
 	assert.EqualError(t, err, mockError.Error())
 	roomRepository.AssertExpectations(t)
 }
