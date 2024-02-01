760d56919ef74fbefb6741cb817ecb70216ecd0e
jibaru <ignacioruedaboada@hotmail.com>
2024-01-31 20:38:35 -0500

    feat: add query filters

--- internal/app/domain/repositories/arguments.go
+++ internal/app/domain/repositories/arguments.go
@@ -1,6 +1,34 @@
 package repositories
 
+const (
+	AndLogicalOperator LogicalOperator = "AND"
+	OrLogicalOperator  LogicalOperator = "OR"
+)
+
+const (
+	LikeComparisonOperator  ComparisonOperator = "LIKE"
+	EqualComparisonOperator ComparisonOperator = "="
+)
+
+type ComparisonOperator string
+type LogicalOperator string
+
 type PageFilter struct {
 	Offset int
 	Limit  int
+}
+
+type QueryFilter struct {
+	ConditionGroups []ConditionGroup
+}
+
+type ConditionGroup struct {
+	Operator   LogicalOperator
+	Conditions []Condition
+}
+
+type Condition struct {
+	Field    string
+	Operator ComparisonOperator
+	Value    interface{}
 }
