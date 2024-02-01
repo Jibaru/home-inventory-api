fe4af79ba34a7c1c145a7dd348f85e5585f056b7
jibaru <ignacioruedaboada@hotmail.com>
2024-01-31 20:41:24 -0500

    feat: update room repository interface

--- internal/app/domain/repositories/room.go
+++ internal/app/domain/repositories/room.go
@@ -6,11 +6,15 @@ import (
 )
 
 var (
-	ErrCanNotCreateRoom            = errors.New("can not create room")
-	ErrCanNotCheckIfRoomExistsByID = errors.New("can not check if room exists by id")
+	ErrCanNotCreateRoom               = errors.New("can not create room")
+	ErrCanNotCheckIfRoomExistsByID    = errors.New("can not check if room exists by id")
+	ErrRoomRepositoryCanNotGetRooms   = errors.New("can not get rooms")
+	ErrRoomRepositoryCanNotCountRooms = errors.New("can not count rooms")
 )
 
 type RoomRepository interface {
 	Create(room *entities.Room) error
 	ExistsByID(id string) (bool, error)
+	GetByQueryFilters(queryFilter QueryFilter, pageFilter *PageFilter) ([]*entities.Room, error)
+	CountByQueryFilters(queryFilter QueryFilter) (int64, error)
 }
