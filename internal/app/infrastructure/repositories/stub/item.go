94d08bb3e89c830dcd60010bea22458f7f468ccf
ignacior <ignacioruedaboada@hotmail.com>
2024-02-09 11:17:27 -0500

    feat: update stub item repository

--- internal/app/infrastructure/repositories/stub/item.go
+++ internal/app/infrastructure/repositories/stub/item.go
@@ -2,6 +2,7 @@ package stub
 
 import (
 	"github.com/jibaru/home-inventory-api/m/internal/app/domain/entities"
+	"github.com/jibaru/home-inventory-api/m/internal/app/domain/repositories"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -22,4 +23,24 @@ func (r *ItemRepositoryMock) GetByID(id string) (*entities.Item, error) {
 	}
 
 	return args.Get(0).(*entities.Item), args.Error(1)
+}
+
+func (r *ItemRepositoryMock) GetByQueryFilters(
+	queryFilter repositories.QueryFilter,
+	pageFilter *repositories.PageFilter,
+) ([]*entities.Item, error) {
+	args := r.Called(queryFilter, pageFilter)
+
+	if data := args.Get(0); data != nil {
+		return data.([]*entities.Item), args.Error(1)
+	}
+
+	return nil, args.Error(1)
+}
+
+func (r *ItemRepositoryMock) CountByQueryFilters(
+	queryFilter repositories.QueryFilter,
+) (int64, error) {
+	args := r.Called(queryFilter)
+	return args.Get(0).(int64), args.Error(1)
 }
