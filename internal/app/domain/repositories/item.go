8c2f6781f2949d4938898e10a3234559a8866082
ignacior <ignacioruedaboada@hotmail.com>
2024-02-09 11:16:50 -0500

    feat: update item repository interface

--- internal/app/domain/repositories/item.go
+++ internal/app/domain/repositories/item.go
@@ -6,11 +6,15 @@ import (
 )
 
 var (
-	ErrItemRepositoryCanNotCreateItem = errors.New("can not create item")
-	ErrItemRepositoryItemNotFound     = errors.New("item not found")
+	ErrItemRepositoryCanNotCreateItem          = errors.New("can not create item")
+	ErrItemRepositoryItemNotFound              = errors.New("item not found")
+	ErrItemRepositoryCanNotGetByQueryFilters   = errors.New("can not get by query filters")
+	ErrItemRepositoryCanNotCountByQueryFilters = errors.New("can not count by query filters")
 )
 
 type ItemRepository interface {
 	Create(item *entities.Item) error
 	GetByID(id string) (*entities.Item, error)
+	GetByQueryFilters(queryFilter QueryFilter, pageFilter *PageFilter) ([]*entities.Item, error)
+	CountByQueryFilters(queryFilter QueryFilter) (int64, error)
 }
