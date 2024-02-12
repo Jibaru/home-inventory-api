e8818e822e8eb06fee834f4f9f4b585523262ed5
ignacior <ignacioruedaboada@hotmail.com>
2024-02-12 09:53:05 -0500

    feat: update box repository interface

--- internal/app/domain/repositories/box.go
+++ internal/app/domain/repositories/box.go
@@ -6,14 +6,17 @@ import (
 )
 
 var (
-	ErrBoxRepositoryBoxItemNotFound             = errors.New("box item not found")
-	ErrBoxRepositoryCanBotCreateBoxItem         = errors.New("can not create box item")
-	ErrBoxRepositoryCanNotCreateBox             = errors.New("can not create box")
-	ErrBoxRepositoryCanNotCreateBoxTransaction  = errors.New("can not create box transaction")
-	ErrBoxRepositoryCanNotDeleteBoxItem         = errors.New("can not delete box item")
-	ErrBoxRepositoryCanNotUpdateBoxItem         = errors.New("can not update box item")
-	ErrorBoxRepositoryCanNotGetByQueryFilters   = errors.New("can not get by query filters")
-	ErrorBoxRepositoryCanNotCountByQueryFilters = errors.New("can not count by query filters")
+	ErrBoxRepositoryBoxItemNotFound                    = errors.New("box item not found")
+	ErrBoxRepositoryCanBotCreateBoxItem                = errors.New("can not create box item")
+	ErrBoxRepositoryCanNotCreateBox                    = errors.New("can not create box")
+	ErrBoxRepositoryCanNotCreateBoxTransaction         = errors.New("can not create box transaction")
+	ErrBoxRepositoryCanNotDeleteBox                    = errors.New("can not delete box")
+	ErrBoxRepositoryCanNotDeleteBoxItem                = errors.New("can not delete box item")
+	ErrBoxRepositoryCanNotDeleteBoxItemsByBoxID        = errors.New("can not delete box items by box id")
+	ErrBoxRepositoryCanNotDeleteBoxTransactionsByBoxID = errors.New("can not delete box transactions by box id")
+	ErrBoxRepositoryCanNotUpdateBoxItem                = errors.New("can not update box item")
+	ErrorBoxRepositoryCanNotCountByQueryFilters        = errors.New("can not count by query filters")
+	ErrorBoxRepositoryCanNotGetByQueryFilters          = errors.New("can not get by query filters")
 )
 
 type BoxRepository interface {
@@ -25,4 +28,7 @@ type BoxRepository interface {
 	DeleteBoxItem(boxID string, itemID string) error
 	GetByQueryFilters(queryFilter QueryFilter, pageFilter *PageFilter) ([]*entities.Box, error)
 	CountByQueryFilters(queryFilter QueryFilter) (int64, error)
+	DeleteBoxItemsByBoxID(boxID string) error
+	DeleteBoxTransactionsByBoxID(boxID string) error
+	Delete(id string) error
 }
