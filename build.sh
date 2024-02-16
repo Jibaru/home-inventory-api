95fca2a5509065ecb76a70c17a1ca424c7a5cad6
jibaru <ignacioruedaboada@hotmail.com>
2024-02-15 22:22:04 -0500

    chore: add build.sh

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+APP_NAME="home-inventory-api"
+VERSION="0.0.1"
+OUTPUT_DIR="bin"
+MAIN_FILE="./cmd/app/"
+
+PLATFORMS=("windows/386" "windows/amd64" "linux/386" "linux/amd64" "darwin/amd64" "linux/arm")
+
+# Clean and create the output directory
+rm -rf "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
+touch "$OUTPUT_DIR/.gitkeep"
+
+# Iterate through platforms and compile the application
+for PLATFORM in "${PLATFORMS[@]}"; do
+    GOOS=${PLATFORM%/*}
+    GOARCH=${PLATFORM#*/}
+    OUTPUT_NAME="$OUTPUT_DIR/${APP_NAME}_${VERSION}_${GOOS}_${GOARCH}"
+
+    # Compile for the current platform
+    if [[ "$GOOS" == "windows" ]]; then
+        env GOOS=$GOOS GOARCH=$GOARCH go build -o "$OUTPUT_NAME".exe "$MAIN_FILE"
+    else
+        env GOOS=$GOOS GOARCH=$GOARCH go build -o "$OUTPUT_NAME" "$MAIN_FILE"
+    fi
+
+    # Check if the compilation was successful
+    if [ $? -eq 0 ]; then
+        echo "Compilation successful for $PLATFORM"
+    else
+        echo "Error compiling for $PLATFORM"
+    fi
+done
+
+echo "Compilation complete. Binaries are available in the $OUTPUT_DIR directory."
