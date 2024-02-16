a86be6e20a8b0abd19e71e8954591f4c16b3cbdf
jibaru <ignacioruedaboada@hotmail.com>
2024-02-15 21:22:06 -0500

    feat: update user repository interface

--- internal/app/domain/repositories/user.go
+++ internal/app/domain/repositories/user.go
@@ -6,12 +6,14 @@ import (
 )
 
 var (
-	ErrUserRepositoryCanNotCreateUser = errors.New("can not create user")
-	ErrUserRepositoryCanNotGetUser    = errors.New("can not get user")
-	ErrUserRepositoryUserNotFound     = errors.New("user not found")
+	ErrUserRepositoryCanNotCreateUser     = errors.New("can not create user")
+	ErrUserRepositoryCanNotGetUser        = errors.New("can not get user")
+	ErrUserRepositoryCanNotGetUserByBoxID = errors.New("can not get user by box id")
+	ErrUserRepositoryUserNotFound         = errors.New("user not found")
 )
 
 type UserRepository interface {
 	Create(user *entities.User) error
 	FindByEmail(email string) (*entities.User, error)
+	GetUserByBoxID(boxID string) (*entities.User, error)
 }
